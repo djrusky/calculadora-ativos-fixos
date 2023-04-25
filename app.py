56ef16e7b5994f44d467685e0b04833ebc7c549a
Matheus Furlanetto <mfurlanetto_personal+github@pm.me>
2023-04-25 11:44:10 -0300

    [FIX]: correções de rota

--- app.py
+++ app.py
@@ -1,66 +1,78 @@
-from flask import Flask, render_template, request
-import FIA
-import io
-import base64
 import matplotlib.pyplot as plt
+from flask import Flask, render_template, request
+
+import lib.FIA as FIA
 
 app = Flask(__name__)
 
-# Global variables
-taxas_retorno = []
-precos = []
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("calculator.html")
 
-@app.route('/price', methods=['POST'])
-def calcular_preco():
-    maturidade = float(request.form['maturidade'])
-    ytm = float(request.form['ytm'])
-    cupom = float(request.form['cupom'])
-    valor_face = float(request.form['valor_face'])
-    compounding = float(request.form['compounding'])
+
+@app.route("/example")
+def example():
+    return render_template("example.html")
+
+
+@app.route("/calculator/fair-price", methods=["POST"])
+def fair_price():
+    maturidade = float(request.form["maturidade"])
+    ytm = float(request.form["ytm"])
+    cupom = float(request.form["cupom"])
+    valor_face = float(request.form["valor_face"])
+    compounding = float(request.form["compounding"])
     print(compounding)
 
-    titulo = FIA.create_coupon_bond(maturity=maturidade, face=valor_face, rate=cupom, frequency=1)
+    titulo = FIA.create_coupon_bond(
+        maturity=maturidade, face=valor_face, rate=cupom, frequency=1
+    )
     preco = titulo.price(ytm, compounding=compounding)
 
-    return render_template('index.html', preco_justo=preco, ytm_justo=ytm)
+    return render_template("calculator.html", preco=preco)
 
-@app.route('/ytm', methods=['POST'])
-def calcular_ytm():
-    maturidade = float(request.form['maturidade'])
-    preco = float(request.form['preco'])
-    cupom = float(request.form['cupom'])
-    valor_face = float(request.form['valor_face'])
 
-    titulo = FIA.create_coupon_bond(maturity=maturidade, face=valor_face, rate=cupom, frequency=1)
+@app.route("/calculator/ytm", methods=["POST"])
+def ytm():
+    maturidade = float(request.form["maturidade"])
+    preco = float(request.form["preco"])
+    cupom = float(request.form["cupom"])
+    valor_face = float(request.form["valor_face"])
+
+    titulo = FIA.create_coupon_bond(
+        maturity=maturidade, face=valor_face, rate=cupom, frequency=1
+    )
     ytm = titulo.YTM(preco)
 
-    return render_template('index.html', preco_ytm=ytm)
+    return render_template("calculator.html", ytm=ytm)
 
-@app.route('/grafico', methods=['POST'])
+
+@app.route("/calculator/graph", methods=["POST"])
 def grafico():
     # Dados do título original
-    maturidade = float(request.form['maturidade'])
-    cupom = float(request.form['cupom'])
-    valor_face = float(request.form['valor_face'])
-    compounding = float(request.form['compounding'])
-    
-    titulo1 = FIA.create_coupon_bond(maturity=maturidade, face=valor_face, rate=cupom, frequency=1)
-    
-    ytm1 = float(request.form['ytm'])
+    maturidade = float(request.form["maturidade"])
+    cupom = float(request.form["cupom"])
+    valor_face = float(request.form["valor_face"])
+    compounding = float(request.form["compounding"])
+
+    titulo1 = FIA.create_coupon_bond(
+        maturity=maturidade, face=valor_face, rate=cupom, frequency=1
+    )
+
+    ytm1 = float(request.form["ytm"])
     preco_justo1 = titulo1.price(ytm1, compounding=1)
 
     # Dados do título com curva de juros ajustada
-    ytm2 = float(request.form['ytm2'])
-    if int(request.form['periodo']) == None:
+    ytm2 = float(request.form["ytm2"])
+    if int(request.form["periodo"]) == None:
         periodo = 0
     else:
-        periodo = int(request.form['periodo'])
+        periodo = int(request.form["periodo"])
 
-    titulo2 = FIA.create_coupon_bond(maturity=maturidade-periodo, face=valor_face, rate=cupom, frequency=1)
+    titulo2 = FIA.create_coupon_bond(
+        maturity=maturidade - periodo, face=valor_face, rate=cupom, frequency=1
+    )
     preco_justo2 = titulo2.price(ytm2, compounding=1)
 
     # Cria as curvas
@@ -70,25 +82,28 @@ def grafico():
 
     # Cria o gráfico
     fig, ax = plt.subplots()
-    ax.plot(ytm1, preco_justo1, label='Curva original', color='purple')
-    ax.plot(ytm2, preco_justo2, label='Nova curva', color='blue')
-    ax.plot(ytm1, preco_justo1, 'ro', label='Preço original', color='purple')
-    ax.plot(ytm2, preco_justo2, 'go', label='Novo preço', color='blue')
-    ax.set_xlabel('YTM')
-    ax.set_ylabel('Preço')
+    ax.plot(ytm1, preco_justo1, label="Curva original", color="purple")
+    ax.plot(ytm2, preco_justo2, label="Nova curva", color="blue")
+    ax.plot(ytm1, preco_justo1, "ro", label="Preço original", color="purple")
+    ax.plot(ytm2, preco_justo2, "go", label="Novo preço", color="blue")
+    ax.set_xlabel("YTM")
+    ax.set_ylabel("Preço")
     ax.legend()
 
+    return render_template("calculator.html")
 
-    # Cria a tabela de variação de preços
-@app.route('/table', methods=['POST'])
+
+@app.route("/calculator/price-table", methods=["POST"])
 def calcular_tabela():
-    maturidade = float(request.form['maturidade'])
-    cupom = float(request.form['cupom'])
-    valor_face = float(request.form['valor_face'])
-    compounding = float(request.form['compounding'])
+    maturidade = float(request.form["maturidade"])
+    cupom = float(request.form["cupom"])
+    valor_face = float(request.form["valor_face"])
+    compounding = float(request.form["compounding"])
     taxas = [0.01 * i for i in range(1, 11)]  # taxa de juros de 1% a 10%
 
-    titulo = FIA.create_coupon_bond(maturity=maturidade, face=valor_face, rate=cupom, frequency=1)
+    titulo = FIA.create_coupon_bond(
+        maturity=maturidade, face=valor_face, rate=cupom, frequency=1
+    )
     preco_base = titulo.price(0.0, compounding=compounding)
 
     precos = []
@@ -99,13 +114,6 @@ def calcular_tabela():
         precos.append(preco)
         variacoes.append(variacao)
 
-    # Converte o gráfico
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    graph_url = base64.b64encode(img.getvalue()).decode()
-
-    # Renderiza o gráfico e a tabela
-
-    return render_template('index.html', taxas=taxas, precos=precos, variacoes=variacoes)
-
+    return render_template(
+        "calculator.html", taxas=taxas, precos=precos, variacoes=variacoes
+    )
